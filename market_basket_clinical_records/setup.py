67c74efc5042826ea1156290f675d4e0477dc1e5
kingulec <kinga_kociszewska@interia.pl>
2022-06-14 22:15:51 +0200

    add requirements file and setup file

---
+++ market_basket_clinical_records/setup.py
@@ -0,0 +1,19 @@
+"""
+This is setup file for market basket analysis project
+
+requirements.txt is a file including the required packages
+
+* in order to install your package run the fallowing
+from inside the market_basket_analysis directory
+    'pip install -e .'
+* in order to install just required packages run
+    'pip install -r "requirement,txt"'
+
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='',
+    packages=find_packages(),
+    install_requires=[]
+)
